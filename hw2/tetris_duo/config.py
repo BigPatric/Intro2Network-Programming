5acbe91a11a793ad585558a40e75c383bc32f23c
BigPatric <dinosourgg@gmail.com>
2025-11-17 14:44:28 +0800

    ip

--- hw2/tetris_duo/config.py
+++ hw2/tetris_duo/config.py
@@ -1,17 +1,17 @@
 # Global configuration for server/client hosts and ports
 # Modify these values to relocate services without touching source code.
 
-LOBBY_HOST = '127.0.0.1'
+LOBBY_HOST = '140.113.17.12'
 LOBBY_PORT = 10000
 
-DB_HOST = '127.0.0.1'
+DB_HOST = '140.113.17.12'
 DB_PORT = 10001
 
 # Game server dynamic port range
 GAME_PORT_MIN = 10002
 GAME_PORT_MAX = 20000
-GAME_BIND_HOST = '127.0.0.1'  # host used by game server to bind
-GAME_CONNECT_HOST = '127.0.0.1'  # host used by client to connect
+GAME_BIND_HOST = '140.113.17.12'  # host used by game server to bind
+GAME_CONNECT_HOST = '140.113.17.12'  # host used by client to connect
 
 # Future expansion placeholders
 # TLS_ENABLED = False
